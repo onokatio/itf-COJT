c06e72697e824d00d6d1964418650eeafa5bf1b3
onokatio <onokatio@gmail.com>
2021-06-25 16:01:58 +0900

    update

---
+++ COJT/pattern/pattern.cache/ip/2019.1/5baaf37d1d650bd7/design_1_util_reduced_logic_0_0_stub.v
@@ -0,0 +1,21 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+// Date        : Fri Jun 25 15:11:17 2021
+// Host        : archlinux running 64-bit Arch Linux
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ design_1_util_reduced_logic_0_0_stub.v
+// Design      : design_1_util_reduced_logic_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "util_reduced_logic_v2_0_4_util_reduced_logic,Vivado 2019.1" *)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(Op1, Res)
+/* synthesis syn_black_box black_box_pad_pin="Op1[0:0],Res" */;
+  input [0:0]Op1;
+  output Res;
+endmodule
---
+++ COJT/pattern/pattern.srcs/sources_1/bd/design_1/ipshared/23ce/hdl/syncgen.v
@@ -0,0 +1,81 @@
+//-----------------------------------------------------------------------------
+// Title       : 同期信号生成（受講者設計対象）
+// Project     : pattern
+// Filename    : syncgen.v
+//-----------------------------------------------------------------------------
+// Description :
+// 
+//-----------------------------------------------------------------------------
+// Revisions   :
+// Date        Version  Author        Description
+// 202?/??/??  1.00     ???????????   Created
+//-----------------------------------------------------------------------------
+
+
+module syncgen(
+    input               DCLK,
+    input               DRST,
+    input       [1:0]   RESOL,
+    output  reg         DSP_HSYNC_X,
+    output  reg         DSP_VSYNC_X,
+    output  reg         DSP_preDE,
+    output  reg [10:0]  HCNT,
+    output  reg [10:0]  VCNT
+);
+
+`include "syncgen_param.vh"
+
+assign RESOL = P_RESOL_VGA;
+
+reg [1:0] STATE_V;
+parameter SV_VFP = 2'b00;
+parameter SV_VPW = 2'b01;
+parameter SV_VBP = 2'b10;
+parameter SV_VDO = 2'b11;
+
+reg [1:0] STATE_H;
+parameter SH_HFP = 2'b00;
+parameter SH_HPW = 2'b01;
+parameter SH_HBP = 2'b10;
+parameter SH_HDO = 2'b11;
+
+reg [HSC:0] DCLK_count = 0'b0;
+
+always @(posadge DCLK) begin
+	DCLK_count = DCLK_count + 0'b1;
+	if (STATE_H == SH_HFP && DCLK_count == HFP) begin
+		STATE_H <= SH_HPW;
+		DCLK_count <= 0'b0;
+	end
+	if (STATE_H == SH_HPW && DCLK_count == HPW) begin
+		STATE_H <= SH_HBP;
+		DCLK_count <= 0'b0;
+	end
+	if (STATE_H == SH_HBP && DCLK_count == HBP) begin
+		STATE_H <= SH_HDO;
+		DCLK_count <= 0'b0;
+	end
+	if (STATE_H == SH_HDO && DCLK_count == HDO) begin
+		STATE_H <= SH_HFP;
+		DCLK_count <= 0'b0;
+	end
+end
+
+always @(posedge STATE_H == SH_HFP) begin
+	HCNT = HCNT + 
+end
+
+always @(posadge DCLK) begin
+end
+
+
+reg HFP_count = 0;
+reg HPW_count = 0;
+reg HBP_count = 0;
+reg HDO_count = 0;
+
+reg VFP_count = 0;
+reg VPW_count = 0;
+reg VBP_count = 0;
+reg VDO_count = 0;
+endmodule
